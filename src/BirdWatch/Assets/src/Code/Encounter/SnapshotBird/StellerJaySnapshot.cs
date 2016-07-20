8429a9fa8d252f19e29283f218b7265e54c7b537
harjup <pikmin@gmail.com>
2016-07-19 22:58:09 -0400

    Add empty snapshot behaviors for StellerJay and WilsonSnipe, include comments for general behavior

---
+++ src/BirdWatch/Assets/src/Code/Encounter/SnapshotBird/StellerJaySnapshot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StellerJaySnapshot : SnapshotBird, ISnapshotBird
+{
+    void Update()
+    {
+        // Quickly swoop onto the screen, hop around.
+        // On picture taken, fly off screen, pause, fly onto another spot and start hopping around.
+        // Rinse and repeat.
+    }
+
+    public void OnPictureTaken()
+    { 
+
+    }
+}
---
+++ src/BirdWatch/Assets/src/Code/Encounter/SnapshotBird/WilsonSnipeSnapshot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WilsonSnipeSnapshot : SnapshotBird, ISnapshotBird
+{
+    void Start()
+    {
+        // Moves ziggy zaggy along a river bank. Need a river bank graphic.
+        // On picture taken they can move left / right out of frame, then keep ziggy zagging.
+    }
+
+    public void OnPictureTaken()
+    {
+
+    }
+}
