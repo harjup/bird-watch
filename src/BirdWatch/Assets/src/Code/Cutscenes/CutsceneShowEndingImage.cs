71be02bb3b5a9871d344127a0e20e5927317c5b8
harjup <pikmin@gmail.com>
2016-07-26 14:17:33 -0400

    Show quit button at end of game, add tutorial text for other encounter buttons

---
+++ src/BirdWatch/Assets/src/Code/Cutscenes/CutsceneShowEndingImage.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Yarn.Unity;
+
+public class CutsceneShowEndingImage : MonoBehaviour
+{
+    private GameObject _thanksForPlaying;
+    private GameObject _quitButton;
+
+    void Awake()
+    {
+        _thanksForPlaying = transform.FindChild("thanks-for-playing").gameObject;
+
+        _thanksForPlaying.SetActive(false);
+
+        _quitButton = transform.FindChild("quit-button").gameObject;
+
+        _quitButton.SetActive(false);
+    }
+
+    [YarnCommand("show")]
+    public void Show()
+    {
+        _thanksForPlaying.SetActive(true);
+        _quitButton.SetActive(true);
+    }
+}
---
+++ src/BirdWatch/Assets/src/Code/Cutscenes/QuitGameButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGameButton : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
